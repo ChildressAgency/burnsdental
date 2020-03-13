0de8ed8cabfc75aa6627baf1645c1b324c8b550c
childressdev <jcampbell@childressagency.com>
2020-03-13 13:54:23 -0400

    added

---
+++ wp-theme-files/front-page.php
@@ -0,0 +1,217 @@
+<?php get_header(); ?>
+  <main id="main">
+    <section id="hp-about" class="styled-border styled-border-full" style="background-image:url(../wp-theme-files/images/woman-smiling-braces.jpg);">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-7">
+            <h2 class="text-on-text styled-border">WELCOME<small data-aos="fade-up" data-aos-delay="1000">Who we are</small></h2>
+            <p>Our doors are always open to new patients who are ready to receive the best in dental care. With our array of exceptional dental services available, it's no surprise we are regularly expanding our family of patients.</p>
+            <p>Our quality-oriented staff is skilled at treating patients from all dental backgrounds and make every effort to ensure your visit is comfortable. We provide superior care for achieving excellence in improved dental health. Dr. Preston Burns and his friendly team welcome you!</p>
+            <img src="https://via.placeholder.com/185x165?text=Logo" class="alignleft" alt="" />
+            <img src="https://via.placeholder.com/185x165?text=Logo" class="alignleft" alt="" />
+            <img src="https://via.placeholder.com/185x165?text=Logo" class="alignleft" alt="" />
+          </div>
+          <div class="col-lg-5">
+            <img src="../wp-theme-files/images/over-forty-years.png" class="img-fluid d-block mx-auto" alt="Over 40 Years of healthy smiles being made" />
+          </div>
+        </div>
+      </div>
+      <div class="overlay white-overlay"></div>
+    </section>
+
+    <section id="hp-modern">
+      <div class="container-fluid">
+            <h2 class="text-on-text styled-border">WE'RE MODERN<small data-aos="fade-up" data-aos-delay="1000">Learn more about us</small></h2>
+            <p>We have been assisting the Fredericksburg, Virginia area for over forty years with our families practice. <a href="#">We have some of the most modern dental technologies</a> readily available at our office to help with any dental needs. We offer services from routine check ups to implants.</p>
+            <p><strong><em>We offer:</em></strong></p>
+            <ul>
+              <li>List of machines that they want to be listed for technological advancement</li>
+            </ul>
+            <img src="https://via.placeholder.com/600x365?text=Video+Placeholder" class="alignnone" alt="" />
+      </div>
+    </section>
+
+    <section id="meet-the-doctor" style="background-image:url(../wp-theme-files/images/doctor-gradient.jpg);">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6" data-aos="fade-up">
+            <h2>Meet the Doctor</h2>
+            <h4>Dr. G Preston Burns III</h4>
+            <p>Our doors are always open to new patients who are ready to receive the best in dental care. With our array of exceptional dental services available, it's no surprise we are regularly expanding our family of patients.</p>
+            <a href="#" class="btn-main btn-large">Learn More</a>
+          </div>
+          <div class="col-lg-6">
+            <!-- need to redo background image and get img of just doctor -->
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <section id="hp-services">
+      <div class="container-fluid">
+        <h2 class="text-on-text styled-border text-center">SERVICES<small data-aos="fade-up" data-aos-delay="1000">What we do</small></h2>
+        <div class="hp-services d-flex justify-content-between flex-wrap">
+            <div id="preventative-care" class="hp-service">
+              <h3>Preventative Card</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+
+            <div id="family-dentistry" class="hp-service">
+              <h3>Family Dentistry</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>              
+            </div>
+
+            <div id="cosmetic-dentistry" class="hp-service">
+              <h3>Cosmetic Dentistry</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+
+            <div id="tooth-replacement" class="hp-service">
+              <h3>Tooth Replacement</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+
+            <div id="extractions-preservations" class="hp-service">
+              <h3>Extractions & Preeservation</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+
+            <div id="orthodontics" class="hp-service">
+              <h3>Orthodontics</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>            
+            </div>
+
+            <div id="sedation-dentistry" class="hp-service">
+              <h3>Sedation Dentistry</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+
+            <div id="dental-technology" class="hp-service">
+              <h3>Dental Technology</h3>
+              <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh erismod tincidunt ut laoreet dolore. Lorem ipsum dolor sit amer, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat colutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nusl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et ac.</p>
+              <a href="#" class="btn-main">Learn More</a>
+            </div>
+        </div>
+      </div>
+    </section>
+
+    <setion id="hp-team">
+      <div class="container-fluid">
+        <h2 class="text-on-text styled-border">OUR TEAM<small>Nice to meet your</small></h2>
+      </div>
+
+      <div class="hp-team-members" style="background-image:url(../wp-theme-files/images/blurry-dentist-chair.jpg);">
+        <div class="container-fluid">
+          <ul class="team-member-list">
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/burns.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Dr. Burns<small>Dentist</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/karns.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Dr. Karns<small>Dentist</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/karen-k.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Karen<small>Dental Hygienist</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Wendy.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Wendy<small>Dental Hygienist</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Candice.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Candice<small>Dental Hygienist</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/karen-e.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Karen<small>Dental Assistant</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Elaine.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Elaine<small>Dental Assistant</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Tambra.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Tambra<small>Hygiene Assistant</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Cathy-N.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Cathy<small>Patient Coordinator</small></h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Rebecca.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Rebecca</h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Julie.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Julie</h4>
+                </a>
+              </div>
+            </li>
+            <li>
+              <div class="team-member">
+                <img src="../wp-theme-files/images/team/Blair.jpg" class="img-fluid d-block mx-auto" alt="" />
+                <a href="#">
+                  <h4>Blair<small>Office Manager</small></h4>
+                </a>
+              </div>
+            </li>
+          </ul>
+          <p class="text-center">
+            <a href="#" class="btn-main btn-x-large">The Team</a>
+          </p>
+        </div>
+      </div>
+    </setion>
+  </main>
+<?php get_footer();
