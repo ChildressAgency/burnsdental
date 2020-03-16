d1d7bbb18cebd775248815c296302c666ed18304
childressdev <jcampbell@childressagency.com>
2020-03-16 11:13:52 -0400

    added

---
+++ wp-theme-files/partials/blocks/forms_list.php
@@ -0,0 +1,13 @@
+<?php if(have_rows('forms')): ?>
+  <div class="form-list-box">
+    <h3><?php the_field('box_title'); ?></h3>
+    <div class="form-list">
+      <ul>
+        <?php while(have_rows('forms')): the_row(); ?>
+          <?php $form = get_sub_field('form'); ?>
+          <li><a href="<?php echo esc_url($form['url']); ?>"><?php echo $form['title'] ? esc_html($form['title']) : esc_html($form['filename']); ?></a></li>
+        <?php endwhile; ?>
+      </ul>
+    </div>
+  </div>
+<?php endif;
