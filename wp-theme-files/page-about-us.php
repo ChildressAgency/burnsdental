01159d078bb6b4e1e671ce2ee6ae12e757d538a5
childressdev <jcampbell@childressagency.com>
2020-03-16 10:36:07 -0400

    added

--- wp-theme-files/page-about-us.php
+++ wp-theme-files/page-about-us.php
@@ -4,12 +4,12 @@
       <div class="container-fluid">
         <div class="row">
           <div class="col-lg-7">
-            <h2 class="text-on-text styled-border">ABOUT US<small data-aos="fade-up" data-aos-delay="1000">Our Story</small></h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum lacus elit, bibendum sed condimentum sagittis, vestibulum eu mauris. Morbi ac est eget justo feugiat lacinia. Sed sodales luctus lectus, a imperdiet arcu condimentum vel. Sed faucibus quam sed sem hendrerit euismod. Nulla vehicula quam sodales justo pharetra, vel ullamcorper metus tincidunt. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec convallis nisl est, et viverra felis rutrum eget.</p>
-            <p>Aliquam aliquet elementum sapien, sit amet pulvinar lectus sodales at. Nunc nec quam venenatis, ultricies ex vitae, blandit ipsum. Proin accumsan metus nisl. Curabitur pulvinar condimentum neque, in consectetur mi iaculis quis. Maecenas nec sapien dui. Ut ultrices, ex a imperdiet blandit, ligula libero suscipit elit, at ultrices tellus magna quis lacus. Etiam dictum lacus eu turpis rutrum dignissim. Maecenas sed urna sit amet augue rhoncus fermentum. Integer egestas vestibulum risus, ac porta lacus sagittis eget. Nulla quis mi orci. Curabitur at metus vel eros laoreet iaculis. In lacinia mi ut rutrum feugiat.</p>
+            <h2 class="text-on-text styled-border"><?php the_field('first_section_bottom_title'); ?><small data-aos="fade-up" data-aos-delay="1000"><?php the_field('first_section_top_title'); ?></small></h2>
+            <?php the_field('first_section_content'); ?>
           </div>
           <div class="col-lg-5">
-            <img src="../wp-theme-files/images/junior-senior2.jpg" class="img-fluid d-block mx-auto align-bottom" alt="" />
+            <?php $fist_section_image = get_field('first_section_image'); ?>
+            <img src="<?php echo esc_url($first_section_image['url']); ?>" class="img-fluid d-block mx-auto align-bottom" alt="<?php echo esc_attr($first_section_image['alt']); ?>" />
           </div>
         </div>
       </div>
@@ -18,30 +18,16 @@
 
     <section id="more-about">
       <div class="container-fluid">
-        <h2 class="text-on-text styled-border">DR. PRESTON BURNS<small data-aos-="fade-up" data-aos-delay="1000">Learn More About Our Dentist</small></h2>
+        <h2 class="text-on-text styled-border"><?php the_field('second_section_bottom_title'); ?><small data-aos-="fade-up" data-aos-delay="1000"><?php the_field('second_section_top_title'); ?></small></h2>
         <div class="row">
           <div class="col-md-8">
             <div class="article">
-              <p>Dr. G. Prston Burns III was born in Virginia and grew up in Stafford County. After graduating from Stafford Senior High School he went on to earn a Bachelor of Science in biology from <a href="#" target="_blank">James Madison University</a></p>
-              <p>Following in his father's footstapes, he was accepted early into the <a href="#" target"_blank">MCV Dental School at Virginia Commonwealth University</a> in richmond. He graduated Magna Cum Laude in 1999 and returned to Fredericksburg to join the practice with his father. He received his training for intravenous concvious sedation from the Smerican society for the Advancement of Anesthesia in Dentistry at <a href="#" target="_blank">St. Joseph Medical Center</a> in Patterson, New Jersey.</p>
-              <p>Dr. Bruns moved the practice into the digital age by introducing CAD/CAM computer assisted crowns and onlays with <a href="#" target="_blank">CEREC</a>. He was one of the first dentists in the country to begin using <a href="#" target="_blank">Invisalign</a> (computer genereated clear braces) and now has over 10 years experiecne with the system. Keeping the practice modern, he recently put somputers in the operatories for digital x-ray cpabilities and for future technology.</p>
-              <p>Dr. Burns has been an active member of the dental community. He has been President or Vice-President of the Rappahannock Valley Dental Society for the past 10 years. He is also an officer in the Virginia Association of Dentists for Entravenous Sedation. He gices his time to the community by volunteering hes services at the <a href="#" target="_blank">Moss Free Clinic</a> in Fredericksburg.</p>
-              <p>Dr. Burns acquired ownership of his father's practice in Octborer, 2011 and took on the new practice name of Burn Famiy Dentistry, PC. He resides in stafford wiht his wife Sandy and their two young daughters.</p>
-
-              <h3>Quick Facts About Dr. Burns</h3>
-              <ul>
-                <li>Known as "Dr. Preston" by his patients</li>
-                <li>Took over father's practice in October 2011</li>
-                <li>Bachelor of Sciencein Biology, James Madison University</li>
-                <li>Training from America Society for the Advancement of Anesthesia in Dentistry, St. Joseph's Medical Center</li>
-                <li>10+ years experience wiht Invisalien System</li>
-                <li>Active in several dental societies</li>
-                <li>Proud husband and father of two childres</li>
-              </ul>
+              <?php the_field('second_section_content'); ?>
             </div>
           </div>
           <div class="col-md-4">
-            <img src="../wp-theme-files/images/team/burns.jpg" class="img-fluid d-block mx-auto" alt="" />
+            <?php $second_section_image = get_field('second_section_image'); ?>
+            <img src="<?php echo esc_url($second_section_image['url']); ?>" class="img-fluid d-block mx-auto" alt="<?php echo esc_attr($second_section_image['alt']); ?>" />
           </div>
         </div>
       </div>
