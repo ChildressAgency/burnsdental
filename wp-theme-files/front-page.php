2e5b1ae5f33e31bc24d5c7709a4399403e708003
childressdev <jcampbell@childressagency.com>
2020-03-17 16:39:37 -0400

    fixed typo

--- wp-theme-files/front-page.php
+++ wp-theme-files/front-page.php
@@ -31,7 +31,7 @@
           <div class="col-lg-6" data-aos="fade-up">
             <?php the_field('meet_the_doctor_section_content'); ?>
             <?php
-              $meet_doctor_link = get_field('meet_the_doctor_link');
+              $meet_doctor_link = get_field('meet_the_doctor_section_link');
               if($meet_doctor_link): ?>
                 <a href="<?php echo esc_url($meet_doctor_link['url']); ?>" class="btn-main btn-large"><?php echo esc_html($meet_doctor_link['title']); ?></a>
             <?php endif; ?>
@@ -52,13 +52,15 @@
     $team_members = new WP_Query(array(
       'post_type' => 'team_member',
       'posts_per_page' => -1,
-      'post_status' => 'publish'
+      'post_status' => 'publish',
+      'orderby' => 'menu_order',
+      'order' => 'ASC'
     ));
 
     if($team_members->have_posts()): ?>
       <section id="hp-team">
         <div class="container-fluid">
-          <h2 class="text-on-text styled-border"><?php the_field('team_section_bottom_title'); ?><small data-aos="fade-up" data-aos-delay="1000"><?php the_field('team_section_top_title'); ?></small></h2>
+          <h2 class="text-on-text styled-border"><?php the_field('team_section_title_bottom_title'); ?><small data-aos="fade-up" data-aos-delay="1000"><?php the_field('team_section_title_top_title'); ?></small></h2>
         </div>
 
         <div class="hp-team-members" style="background-image:url(<?php the_field('team_section_background_image'); ?>); <?php the_field('team_section_background_image_css'); ?>">
